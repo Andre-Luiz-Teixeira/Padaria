47b5ee604847e3bff52d41c19971ec93f4762e1a
Andre-Luiz-Teixeira <andrelteixeira@icloud.com>
2019-11-17 22:27:16 -0200

    Construido a pasta View com cliente, produto, vendedor

--- src/Principal/Main.java
+++ src/Principal/Main.java
@@ -5,6 +5,8 @@
  */
 package Principal;
 
+import View.Principal;
+
 /**
  *
  * @author André Teixeira
@@ -15,7 +17,8 @@ public class Main {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Principal frame = new Principal();
+        frame.setVisible(true);
     }
     
 }
